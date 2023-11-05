d1d8db38909e19efe95cef23e9550c7c848c1202
Artem Stepanenko <stepanentem@gmail.com>
2023-11-05 17:36:17 +0200

    added secret

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,6 @@ export const metadata: Metadata = {
     default: "Acme Dashboard",
   },
   description: "The official Next.js Learn Dashboard built with App Router.",
-  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
 };
 
 export default function RootLayout({
